868c35a401ce434be202d04309dfd63976c6905c
the-architect <marcel.scherf@epicteams.com>
2014-06-12 22:08:15 +0200

    add initial spec and rewards_service class

---
+++ lib/rewards_service.rb
@@ -0,0 +1,3 @@
+class RewardsService
+
+end
---
+++ spec/rewards_service_spec.rb
@@ -0,0 +1,9 @@
+require_relative 'spec_helper'
+
+require_relative '../lib/rewards_service'
+
+describe RewardsService do
+  it 'works' do
+    # sanity check
+  end
+end
